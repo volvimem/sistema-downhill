b0d63520fec66e0de0ce7d80ac6a82bcc5f1e92c
DH-PE <copa.pe.dh@gmail.com>
2025-12-30 11:40:12 -0300

    Simplify service worker fetch handling
    Removed caching logic and simplified fetch handling.

--- sw.js
+++ sw.js
@@ -1,22 +1,4 @@
 const CACHE_NAME = 'dh-pe-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './dashboard.html',
-  './carteira.html',
-  './style.css'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
